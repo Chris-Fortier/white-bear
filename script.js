f99a345eacbea466c31a660e5ec3dba8f1705b90
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-07 17:24:36 -0700

    add error message when "back to answer" is clicked

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// brings down success banner when save is clicked
 $("#save-imagery").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
+});
+
+// brings down error banner when back to answer is clicked
+$("#back-to-answer").click(function () {
+   $("#overlay-error").toggleClass("d-flex d-none");
 });
