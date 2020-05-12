d12aab51c3b89cb14bdb01a7ee499122580432d3
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-12 16:53:38 -0700

    move code to js, also made sign up work

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ $("#show-delete").click(function () {
 // $("#bubba").click(function () {
 //    console.log("bubba");
 // });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+   "use strict";
+   window.addEventListener(
+      "load",
+      function () {
+         // Fetch all the forms we want to apply custom Bootstrap validation styles to
+         var forms = document.getElementsByClassName("needs-validation");
+         // Loop over them and prevent submission
+         var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener(
+               "submit",
+               function (event) {
+                  if (form.checkValidity() === false) {
+                     console.log("form is not valid");
+                     event.preventDefault();
+                     event.stopPropagation();
+                  }
+                  form.classList.add("was-validated");
+               },
+               false
+            );
+         });
+      },
+      false
+   );
+})();
