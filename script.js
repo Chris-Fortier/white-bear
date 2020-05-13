7ce184a80df692ed3162b29997feb4acddce755c
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-13 10:44:02 -0700

    change the code for the log in validation

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function charCount() {
    // finds the length of the input string
    // return input.length;
    // console.log("charCount called");
-   string = $("#answerField").val();
+   var string = $("#answerField").val();
    $(".counter").replaceWith(string + "/240");
 }
 
@@ -40,34 +40,65 @@ $("#answerField").keyup(function () {
    $(".counter").text(stringLength + "/240");
 });
 
-console.log("hello")(
-   // Example starter JavaScript for disabling form submissions if there are invalid fields
-   function () {
-      "use strict";
-      window.addEventListener(
-         "load",
-         function () {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName("needs-validation");
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function (
-               form
-            ) {
-               form.addEventListener(
-                  "submit",
-                  function (event) {
-                     if (form.checkValidity() === false) {
-                        console.log("form is not valid");
-                        event.preventDefault();
-                        event.stopPropagation();
-                     }
-                     form.classList.add("was-validated");
-                  },
-                  false
-               );
-            });
-         },
-         false
-      );
+$("#login-button").click(function () {
+   console.log("clicked log in button");
+
+   // check if email is gtg
+   var emailLength = $("#login-email").val().length;
+   if (emailLength == 0) {
+      $("#login-email-error").removeClass("d-none"); // show the error message
+      $("#login-email-error").text("Please enter your email.");
+   } else {
+      $("#login-email-error").addClass("d-none"); // hide the error message
    }
-)();
+
+   // check if password is gtg
+   var passwordLength = $("#login-password").val().length;
+   if (passwordLength < 9) {
+      // password needs to be fixed
+      $("#login-password-error").removeClass("d-none"); // show the error message
+      $("#login-password-error").text(
+         "Your password must be at least 9 characters."
+      );
+
+      if (passwordLength == 0) {
+         // password is empty
+         $("#login-password-error").text("Please enter your password.");
+      }
+   } else {
+      // password is fine
+      $("#login-password-error").addClass("d-none"); // hide the error message
+   }
+});
+
+// console.log("hello")(
+//    // Example starter JavaScript for disabling form submissions if there are invalid fields
+//    function () {
+//       "use strict";
+//       window.addEventListener(
+//          "load",
+//          function () {
+//             // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//             var forms = document.getElementsByClassName("needs-validation");
+//             // Loop over them and prevent submission
+//             var validation = Array.prototype.filter.call(forms, function (
+//                form
+//             ) {
+//                form.addEventListener(
+//                   "submit",
+//                   function (event) {
+//                      if (form.checkValidity() === false) {
+//                         console.log("form is not valid");
+//                         event.preventDefault();
+//                         event.stopPropagation();
+//                      }
+//                      form.classList.add("was-validated");
+//                   },
+//                   false
+//                );
+//             });
+//          },
+//          false
+//       );
+//    }
+// )();
